1873de2351a607533e0aab0c1d01494fac0cf5cf
Kolbeev Grigory <122559765+TOKYOGATSBY@users.noreply.github.com>
2023-03-03 20:19:12 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+//Напишите программу, которая запрашивает у пользователь радиус круга и, используя полученный радиус, вычисляет площадь круга.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    const float number_of_pi {3.14};
+    int radius {};
+
+
+    cout << "Enter the radius\n";
+    cin >> radius;
+
+    const float square {number_of_pi * radius * radius};
+
+    cout << "The square is " << square;
+
+    return 0;
+}
